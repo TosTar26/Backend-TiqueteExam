bee4f726b8eb437c8553af870d92894187affafd
TosTar26 <ozsanchez2004@gmail.com>
2024-06-25 15:53:51 -0600

    Creating Context in DataAccess

--- Tickets_Sist/obj/Debug/net7.0/Tickets_Sist.AssemblyInfo.cs
+++ Tickets_Sist/obj/Debug/net7.0/Tickets_Sist.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tickets_Sist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+501f942bdf7951bfc96597b69af813aaf01e61a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ff97b618c3e430062f4bffa757e922e2bb3df3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tickets_Sist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tickets_Sist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
